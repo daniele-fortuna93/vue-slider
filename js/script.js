195780662d3a63edd37c416d7530141237f35af9
Daniele <bambudio@libero.it>
2021-03-25 16:46:53 +0100

    Aggiunti dots e buttons

--- js/script.js
+++ js/script.js
@@ -8,7 +8,8 @@ var app = new Vue(
         'img/17.jpg',
         'img/8.jpg'
       ],
-      imageIndex: 0
+      imageIndex: 0,
+      inputValue: ''
     },
     methods: {
       nextImg: function () {
@@ -22,8 +23,24 @@ var app = new Vue(
         if ( this.imageIndex < 0 ) {
           this.imageIndex = this.images.length - 1;
         }
-      }
+      },
+      addImg: function () {
+        if ( this.images.length < 8 ) {
+          this.images.push(this.inputValue);
+          this.inputValue = '';
+        } else {
+          this.inputValue = '';
+          alert('Hai raggiunto il numero massimo di immagini da poter inserire.');
+        }
+      },
+      delImg: function () {
+        if ( this.images.length > 1 ) {
+          this.images.pop();
+        } else {
+          alert('Non puoi rimuovere altre immagini');
+        }
 
+      }
     }
   }
 );
